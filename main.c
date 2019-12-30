c6161dc75bfbc9a49953d06a1396e7c6950a4752
Muhammed İsa Akbaba <isa.akbaba03@gmail.com>
2019-12-30 03:02:14 +0300

    read, upper and replace is done

--- main.c
+++ main.c
@@ -24,6 +24,7 @@ typedef struct t_params {
     char **r_lines;
     sem_t *mutex_arr;
     int length;
+    int thread_number;
 } t_params;
 //typedef t_params *tParams;
 
@@ -61,6 +62,7 @@ int main(int argc, char *argv[]) {
     sem_init(&upper_count_mutex, 0, 1);
     sem_init(&replace_count_mutex, 0, 1);
     sem_init(&counter_mutex, 0, 1); //init binary semaphore
+
 /* READ THREAD INITIALIZATION */
     t_params read_param[NUM_READ_THREADS];
     pthread_t readers[NUM_READ_THREADS];
@@ -69,12 +71,17 @@ int main(int argc, char *argv[]) {
         read_param[i].id = i;
         read_param[i].filename = strdup(argv[2]);
         read_param[i].r_lines = read_lines;
+        read_param[i].mutex_arr = mutex_arr;
+        read_param[i].length = line_number;
+        read_param[i].thread_number = NUM_READ_THREADS;
         rc = pthread_create(&readers[i], NULL, get_nth_line, (void *) &read_param[i]);
         if (rc) {
             printf("ERROR; return code from pthread_create() is %d\n", rc);
             exit(-1);
         }
     }
+
+
 
     /* UPPER THREAD INITIALIZATION */
     t_params upper_param[NUM_UPPER_THREADS];
@@ -85,13 +92,16 @@ int main(int argc, char *argv[]) {
         upper_param[m].r_lines = read_lines;
         upper_param[m].mutex_arr = mutex_arr;
         upper_param[m].id = m;
+        upper_param[m].thread_number = NUM_UPPER_THREADS;
         rc1 = pthread_create(&uppers[m], NULL, to_upper, (void *) &upper_param[m]);
         if (rc1) {
-            printf("ERROR; return code from pthread_create() is %d\n", rc);
+            printf("ERROR; return code from pthread_create() is %d\n", rc1);
             exit(-1);
         }
     }
 
+
+/* REPLACE THREAD INITIALIZATION */
     t_params replace_param[NUM_REPLACE_THREADS];
     pthread_t replacer[NUM_REPLACE_THREADS];
     int rc2;
@@ -100,13 +110,13 @@ int main(int argc, char *argv[]) {
         replace_param[a].r_lines = read_lines;
         replace_param[a].mutex_arr = mutex_arr;
         replace_param[a].id = a;
+        replace_param[a].thread_number = NUM_REPLACE_THREADS;
         rc2 = pthread_create(&replacer[a], NULL, replace, (void *) &replace_param[a]);
         if (rc2) {
-            printf("ERROR; return code from pthread_create() is %d\n", rc);
+            printf("ERROR; return code from pthread_create() is %d\n", rc2);
             exit(-1);
         }
     }
-
 
     for (int k = 0; k < NUM_READ_THREADS; ++k) {
         pthread_join(readers[k], NULL);
@@ -121,6 +131,7 @@ int main(int argc, char *argv[]) {
 //        printf("index: %d line: %s\n", l, read_lines[l]);
 //    }
 
+
 /* destroy semaphore */
     sem_destroy(&counter_mutex);
     sem_destroy(&upper_sem);
@@ -130,7 +141,13 @@ int main(int argc, char *argv[]) {
     for (int j = 0; j < line_number; ++j) {
         sem_destroy(&mutex_arr[j]);
     }
-
+    puts("heloğ");
+    for (int m = 0; m < line_number; ++m) {
+        printf("%s\n", read_lines[m]);
+    }
+//    t_params new;
+//    new.r_lines = read_lines;
+//    to_upper(&new);
     return 0;
 }
 
@@ -147,10 +164,12 @@ void *get_nth_line(void *params) {
         /* START CRITICAL SECTION */
         line_no = line_counter;
         index = line_counter;
+        sem_wait(&t_p->mutex_arr[index]);
         line_counter++;
         /* END CRITICAL SECTION */
         sem_post(&counter_mutex); // up semaphore
 
+//        printf("take mutex %d\n", index);
         FILE *file = fopen(filename, "r");
         char line[256];
         int i = 0;
@@ -161,29 +180,27 @@ void *get_nth_line(void *params) {
             if (line_no == 0) {
                 flag = 0;
                 printf("Thread %zu read line: %s", t_p->id, line);
+
                 t_p->r_lines[index] = strdup(line);
+
                 sem_post(&upper_sem);
                 sem_post(&replace_sem);
-//                int sam_val;
-//                sem_getvalue(&upper_sem, &sam_val);
-//                printf("sam value is: %d\n", sam_val);
+
                 break;
             }
             line_no--;
         }
         fclose(file);
-//
-//        printf("Thread %zu read line: %s", t_p->id, line);
-//        t_p->r_lines[index] = strdup(line);
-//        int sam_val;
-//        sem_getvalue(&upper_sem, &sam_val);
-//        printf("sam value is: %d\n", sam_val);
-        if (flag == 1) {
-//            int sam_val;
-//            sem_getvalue(&upper_sem, &sam_val);
-//            printf("sam value is: %d\n", sam_val);
-            pthread_exit(NULL);
+        sem_post(&t_p->mutex_arr[index]);
+
+//        printf("give mutex %d\n", index);
+        if (index > t_p->length - t_p->thread_number - 1) {
+            break;
         }
+//        if (flag == 1) {
+////            sem_post(&t_p->mutex_arr[index]);
+//            pthread_exit(NULL);
+//        }
 //        read_lines2[index] = strdup(line);
 //        sem_post(&arr_mutex);
     }
@@ -210,13 +227,10 @@ void *to_upper(void *params) {
         sem_wait(&upper_sem);
         sem_wait(&upper_count_mutex);
         index = upper_count;
+        sem_wait(&(t_p->mutex_arr[index])); // function critical section starts
         upper_count++;
         sem_post(&upper_count_mutex);
-        //bitiş şartı
-        if (index >= t_p->length) {
-            pthread_exit(NULL);
-        }
-        sem_wait(&(t_p->mutex_arr[index])); // function critical section starts
+
         /*critical section */
 
         char *line = t_p->r_lines[index];
@@ -226,39 +240,62 @@ void *to_upper(void *params) {
             upper[i] = toupper(line[i]);
         }
         upper[i] = '\0';
-        t_p->r_lines[index] = upper;
+        t_p->r_lines[index] = strdup(upper);
+        printf("changed line: %s", upper);
+//        printf("%s", upper);
         sem_post(&(t_p->mutex_arr[index]));// function critical section ends.
+
+        if (index > t_p->length - t_p->thread_number - 1) { //bitiş şartı
+//            printf("thread: %zu is done\n", t_p->id);
+            break;
+            pthread_exit(NULL);
+        }
     }
+    pthread_exit(NULL);
 }
 
 void *replace(void *params) {
     t_params *t_p = (t_params *) params;
     int index;
-    /*critical section*/
-    sem_wait(&replace_sem);
-    sem_wait(&upper_count_mutex);
-    index = replace_count;
-    replace_count++;
-    sem_post(&replace_count_mutex);
-    //bitiş şartı
-    if (index >= t_p->length) {
-        pthread_exit(NULL);
-    }
-    sem_wait(&(t_p->mutex_arr[index])); // function critical section starts
-    /*critical section*/
-
-    char *line = t_p->r_lines[index];
-    char replace[strlen(line) + 1];
-    int i;
-    for (i = 0; i < strlen(line); ++i) {
-        if (isspace(line[i])) {
-            replace[i] = '_';
-            continue;
+    while (1) {
+        /*critical section*/
+        if (replace_count == 0) {
+            sem_wait(&replace_sem);
         }
-        replace[i] = line[i];
+        sem_wait(&replace_count_mutex);
+        index = replace_count;
+        sem_wait(&(t_p->mutex_arr[index])); // function critical section starts
+        replace_count++;
+
+        sem_post(&replace_count_mutex);
+
+        /*critical section*/
+
+        char *line = t_p->r_lines[index];
+        char line_copy[256];
+        strcpy(line_copy, line);
+//        printf("line copy is: %s", line_copy);
+        char replace[256];
+        int i;
+        for (i = 0; i < strlen(line); ++i) {
+            if (isspace(line[i])) {
+                replace[i] = '_';
+                continue;
+            }
+            replace[i] = line[i];
+        }
+        replace[i - 1] = '\0';
+
+        t_p->r_lines[index] = strdup(replace);
+        printf("changed line: %s\n", replace);
+
+        sem_post(&(t_p->mutex_arr[index])); // function critical section ends
+
+        if (index > t_p->length - t_p->thread_number - 1) { //bitiş şartı
+//            printf("thread: %zu is done\n", t_p->id);
+            break;
+            pthread_exit(NULL);
+        }
     }
-    replace[i] = '\0';
-    t_p->r_lines[index] = replace;
-    sem_post(&(t_p->mutex_arr[index])); // function critical section ends
 }
 
